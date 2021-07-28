13d0c42edd037d6430aa7c5ddfd259d69fcb6ba4
Tfauves <tfauvelle@gmail.com>
2021-07-28 14:56:07 -0500

    Dice roll working.

--- src/com/careerDevs/Main.java
+++ src/com/careerDevs/Main.java
@@ -4,5 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
+        Cup cup1 = new Cup();
+        cup1.roll();
+        System.out.println(cup1.displayCup());
+
     }
 }
