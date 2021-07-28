0c2e1da2feab1d225c1ad4ee283e1687d0188885
Tfauves <tfauvelle@gmail.com>
2021-07-28 10:24:27 -0500

    Outlined Cup, Die, Player classes.

--- src/com/careerDevs/Player.java
+++ src/com/careerDevs/Player.java
@@ -1,4 +1,21 @@
 package com.careerDevs;
 
 public class Player {
+    public String name;
+    public Cup cup = new Cup();
+    public int score = 0;
+    //public int roundScore;
+
+    public Player(String name) {
+        this.name = name;
+    }
+
+    public int updateScore() {
+        int roundScore = 0;
+        for (Die die : cup.dice) {
+            roundScore += die.faceUpValue;
+        }
+        score += roundScore;
+        return roundScore;
+    }
 }
